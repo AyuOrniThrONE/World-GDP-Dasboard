c9c6a7e4e8485b32997ac25036c0bf31866de97a
AyuOrniThrONE <ayushprakashthaware@gmail.com>
2026-04-15 01:31:10 +0530

    Fetch and Clean Pipeline

--- data_pipeline/clean_data.py
+++ data_pipeline/clean_data.py
@@ -8,6 +8,6 @@ df["year"] = df["year"].astype(int)
 
 df = df.sort_values(["country", "year"])
 
-df.to_csv("data/gdp_cleaned.csv", index=False)
+df.to_csv("data/gdp_data.csv", index=False)
 
 print("Data cleaned!")
